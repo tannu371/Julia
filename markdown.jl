7b9bd6c0bebc80fcb8db785b964d57e039651860
Tannu Kumari <121978242+tannu371@users.noreply.github.com>
2023-10-14 17:30:57 +0530

    14oct

---
+++ markdown.jl
@@ -0,0 +1,207 @@
+### A Pluto.jl notebook ###
+# v0.19.27
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ d1f9e3b6-88e5-4308-b0af-3361dedb912d
+md"""
+# Markdown
+"""
+
+# ╔═╡ 379dca9d-0e6a-406c-b81c-d567c1cce157
+md"""
+## Inline elements
+**Bold**
+
+*Italics*
+
+`Literals`
+
+``\LaTeX``
+
+"""
+
+# ╔═╡ 19fad75e-b41b-41c2-bb4b-ba59fccc60ee
+md"""
+### Latex
+Start Learning ``\JuLiA``.
+
+``\LaTeX`` with same syntax.
+
+Have fun learning ``JuLiA``.
+
+I'm sure you'll love ``\\JuLiA``.
+"""
+
+# ╔═╡ 84633f7f-0e3b-4537-97f9-3fd3a4024274
+md"""
+### Links
+[Learn Julia](https://julialang.org/learning/)
+"""
+
+# ╔═╡ 4c060e33-2577-4e50-b376-0899f752c88e
+md"""
+### Footnote references
+Karan and Talha both are [^Pisces].
+Tanmay and Debangi both are [^Scorpio].
+
+Happy and peaceful life [^1].
+"""
+
+# ╔═╡ 66a30918-9e55-4018-a6d9-d6655d3096fc
+md"""
+## Toplevel elements
+"""
+
+# ╔═╡ 4d6514bf-9de7-49e2-8925-b09f441e9a69
+md"""
+### Paragraphs
+
+I am nerd. 
+I am weirdo. 
+
+I don't like parties. 
+I don't like to hang out with friends.
+
+"""
+
+# ╔═╡ 083aeb8f-686d-4a16-9e43-2e3620257133
+md"""
+### Headers
+# One ✅
+## Two ✅
+### Three ✅
+#### Four 
+##### Five
+###### Six
+"""
+
+# ╔═╡ 0089950c-e792-48e4-a1a9-7b5078dd49ab
+md"""
+### Code blocks
+	Hi! I am Tannu.
+	And I love julia.
+
+```cpp
+#include<iostream>
+using namespace std;
+int main() {
+cout << "I will be back in a moment." << endl;
+return 0;
+}
+```
+
+```python
+print("I am little wierdo")
+```
+"""
+
+# ╔═╡ 860af7c3-7eb7-4f6d-b990-d1922e1b76bd
+md"""
+### Block quotes
+> Go out in your world
+"""
+
+# ╔═╡ 8644b8ba-dddc-45a1-8af2-b3462399e903
+md"""
+### Images
+![Jai Shree Ram](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIEQmH9dg4oybcD9HNHUlApnq2z-csEb2uIe0TbkT5Ljq4j0gVTptwfTFyY8udn9bdHvg&usqp=CAU)
+"""
+
+# ╔═╡ e209a088-5b32-4d7b-925c-51ef3edf6dde
+md"""
+### List
+Unordered List
+- Check once:
+  - Keys
+  - Charged Phone
+  - Earbuds
+- At Work
+  - Macbook & Charger
+  - Notebook & Pen
+  - Water bottle
+
+Ordered List
+1. Heppy Life
+2. Heppy Life
+3) Heppy Life
+4) Heppy Life
+
+"""
+
+# ╔═╡ 267e67d2-8da3-402f-8276-78e6caee535c
+md"""
+### Display equations
+```math
+f(a) = \int_{-\pi}^{\pi} \frac{1}{1+3(cos\theta)^2} d\theta
+
+```
+``\sqrt[n]{1 + x + x^2 + \ldots}``
+
+``\frac{n!}{k!(n - k)!} = \binom{n}{k}``
+
+"""
+
+# ╔═╡ 5e39f1a3-f900-4785-9e41-281d3d3e50c9
+md"""
+### Footnotes
+[^1]: Be the person you want to be.
+
+[^Pisces]: 19feb-20march 
+[^Scorpio]: 23oct-21nov
+
+"""
+
+# ╔═╡ 51ee7e4a-40ca-4d2c-81b9-f9c0dd2e80f4
+md"""
+### Horizontal rules
+---
+---
+"""
+
+# ╔═╡ b9734611-732b-4104-b271-9ddd8743a1c4
+md"""
+### Tables
+|Date|Time|Subject|
+|----|:----|:----:|
+|17nov|4-7|DM|
+|18nov|4-7|Analog|
+|20nov|4-7|CO|
+|22nov|8-11|PRST|
+|24nov|8-11|DS|
+"""
+
+# ╔═╡ a5f3d8b6-677a-11ee-19fe-7d73310d8409
+md"""
+### Admonitions
+!!! note
+	This is to note
+!!! info
+	This is to inform
+!!! tip
+	This is a tip
+!!! warning
+	This is a warning
+!!! danger
+	There's a danger
+"""
+
+# ╔═╡ Cell order:
+# ╟─d1f9e3b6-88e5-4308-b0af-3361dedb912d
+# ╟─379dca9d-0e6a-406c-b81c-d567c1cce157
+# ╟─19fad75e-b41b-41c2-bb4b-ba59fccc60ee
+# ╟─84633f7f-0e3b-4537-97f9-3fd3a4024274
+# ╟─4c060e33-2577-4e50-b376-0899f752c88e
+# ╟─66a30918-9e55-4018-a6d9-d6655d3096fc
+# ╟─4d6514bf-9de7-49e2-8925-b09f441e9a69
+# ╟─083aeb8f-686d-4a16-9e43-2e3620257133
+# ╟─0089950c-e792-48e4-a1a9-7b5078dd49ab
+# ╟─860af7c3-7eb7-4f6d-b990-d1922e1b76bd
+# ╟─8644b8ba-dddc-45a1-8af2-b3462399e903
+# ╟─e209a088-5b32-4d7b-925c-51ef3edf6dde
+# ╟─267e67d2-8da3-402f-8276-78e6caee535c
+# ╟─5e39f1a3-f900-4785-9e41-281d3d3e50c9
+# ╟─51ee7e4a-40ca-4d2c-81b9-f9c0dd2e80f4
+# ╟─b9734611-732b-4104-b271-9ddd8743a1c4
+# ╟─a5f3d8b6-677a-11ee-19fe-7d73310d8409
